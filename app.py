3b001a0289d4aad3a3c411ad6ba83e8a5b3ae2ad
G13 <g13@BLKBRD.(none)>
2014-02-24 14:01:50 -0300

    atualização

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # -*- coding:cp1252 -*-
 from flask import Flask, render_template, flash, url_for, redirect, request
 
-app = Flask('CadastroDeBares')
+app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def cadastro():
@@ -9,10 +9,11 @@ def cadastro():
 		nome_do_bar = str(request.form.get('nome_do_bar'))
 		endereco = str(request.form.get('endereco'))
 		horario_de_funcionamento = str(request.form.get('horario_de_funcionamento'))
-		especialidade = str(request.form.get('especialidade'))
-		db = open('db.txt', 'a')
-		db.write(nome_do_bar + endereco + horario_de_funcionamento + especialidade)
-		db.close()
+		especialidade_da_casa = str(request.form.get('especialidade_da_casa'))
+		if nome_do_bar and endereco and horario_de_funcionamento and especialidade_da_casa:
+			db = open('db.txt', 'a')
+			db.write(u'%s|%s|%s|%s\n' % (nome_do_bar, endereco, horario_de_funcionamento, especialidade_da_casa))
+			db.close()
 	return render_template('index.html')
 
 if __name__ == "__main__":
